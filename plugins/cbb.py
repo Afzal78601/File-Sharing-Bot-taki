f9ef57cba5e9389764d6374274a1a57d40feb9ea
Afzal78601 <167713227+Afzal78601@users.noreply.github.com>
2024-05-19 09:32:33 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/ifeelscam'>sʜᴀɪᴋʜ ᴀʟɪ</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/InkaLinks'> ᴄʜɪᴘs</a></b>",
+            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/zorosan110'>Roronoa Zoro</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/zorosan110'> ᴄʜɪᴘs</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
-                [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll"),
-                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/publicfille")],
+                [ [ InlineKeyboardButton(" Source code ", url="https://telegra.ph/Source-Code-09-14-3"),
+                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/apna_akatsuki")],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
